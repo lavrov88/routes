8955ae81e618d21afc0dda1d73903ab23d6bcd7c
lavrov88 <a.lavrov88@gmail.com>
2022-05-02 20:50:10 +0300

    routing engine changed to graphhopper, added gh-pages

--- src/components/Map/Routing/Routing.jsx
+++ src/components/Map/Routing/Routing.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import L from "leaflet"
 import "leaflet-routing-machine"
+import "lrm-graphhopper"
 import { useMap } from "react-leaflet"
 
 L.Marker.prototype.options.icon = L.icon({
@@ -14,7 +15,11 @@ const Routing = ({ startPoint, endPoint }) => {
     if (!map) return
 
     const routingControl = L.Routing.control({
-      waypoints: [L.latLng(startPoint.lat, startPoint.lng), L.latLng(endPoint.lat, endPoint.lng)],
+      waypoints: [
+        L.latLng(startPoint.lat, startPoint.lng), 
+        L.latLng(endPoint.lat, endPoint.lng)
+      ],
+      router: L.Routing.graphHopper('4d006104-8fac-4acd-960e-642c8300568f'),
       routeWhileDragging: false
     }).addTo(map)
 
